c3a9c17396c082b6461e5b8f54a136aa8679811b
fat28668221 <96966547+fat28668221@users.noreply.github.com>
2022-11-03 15:50:08 +0800

    11_3

--- My project/Assets/Script/EnemyFireSystem.cs
+++ My project/Assets/Script/EnemyFireSystem.cs
@@ -8,7 +8,17 @@ namespace Justin
     /// </summary>
     public class EnemyFireSystem : FireSystemBase
     {
+        [SerializeField, Header("生成間隔"), Range(0, 3)]
+        private float interval = 1.5f;
 
+            private void Awake()
+        {
+            //呼叫自訂方法
+            //SpawnBullet();
+
+            //延遲
+            InvokeRepeating("SpawnBullet", 0 , interval);
+        }
     }
 }
 
--- My project/Assets/Script/PlayerFireSystem.cs
+++ My project/Assets/Script/PlayerFireSystem.cs
@@ -8,7 +8,15 @@ namespace Justin
     /// </summary>
     public class PlayerFireSystem : FireSystemBase
     {
-
+        //偵測玩家行為
+        private void Update()
+        {
+            //玩家按空白建 生成子彈
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                SpawnBullet();
+            }
+        }
     }
 }
 
